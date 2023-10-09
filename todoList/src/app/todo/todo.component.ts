fccd18547c565df4dcb7892697be14e40073300a
pranav k <pranavkkeloth@gmail.com>
2023-10-09 10:00:22 +0530

    feat: angular todoList completed

--- todoList/src/app/todo/todo.component.ts
+++ todoList/src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { Todo } from '../class/todo';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-todo',
@@ -10,27 +11,21 @@ export class TodoComponent {
   todoValue: String = "";
 
   todoList: Todo[] = [
-    {
-      content: "Todo 1",
-      value: false
-    },
-    {
-      content: "Todo 2",
-      value: false
-    },
-    {
-      content: "Todo 3",
-      value: false
-    },
+    
   ];
 
+  finishedList: Todo[] = [
 
-  constructor(){
+  ]
+
+  constructor(private modalService: NgbModal){
 
   }
 
-  changeTodo(){
-
+  changeTodo(i: number){
+    const item = this.todoList.splice(i,1);
+    console.log(item)
+    this.finishedList.push(item[0])
   }
 
   addTodo(){
@@ -38,4 +33,25 @@ export class TodoComponent {
     this.todoList.push({content:this.todoValue, value: false});
     this.todoValue = "";
   }}
+
+  changeFinished(i: number){
+   const item = this.finishedList.splice(i,1)
+   console.log(item)
+   this.todoList.push(item[0])
+  }
+
+  openModal(content: TemplateRef<Element>, i:number, type: String){
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then(
+      (result)=>{
+        if(type=='todoList'){
+          this.todoList.splice(i,1);
+        }else{
+          this.finishedList.splice(i,1)
+        }
+      },
+      (reason)=>{
+
+      }
+    )
+  }
 }
